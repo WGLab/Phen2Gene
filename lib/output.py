4246c5ab8b349559ba505d1ca1b1c85d7bad3124
Zhao <zhaom3@l-0-01.cm.cluster>
2019-03-18 14:58:50 -0400

    first uploads

---
+++ lib/output.py
@@ -0,0 +1,38 @@
+import numpy as np 
+
+def build_output_file(output_input,method_input,gene_dict):
+
+    with open(output_input + "final_gene_list", "w+") as output_file:
+        output_file.write("Rank\tGene\tID\tScore\tStatus\n")
+        if(len(gene_dict) > 0):
+
+            if(method_input == 'w'):
+                rank = 1
+                highest_score = 1
+                for gene_item in gene_dict.keys():
+                    if(rank == 1):
+                        highest_score = gene_dict[gene_item][1]
+                    output_file.write(str(rank) + "\t" + gene_dict[gene_item][0] + "\t" + str(gene_item) + "\t" + str( np.round(gene_dict[gene_item][1] / highest_score, 6 ) ) + "\t" +gene_dict[gene_item][2] + "\n" )
+                    rank += 1
+            else:
+                gene_ID = list(gene_dict.keys())
+                begin = 0
+                score = gene_dict[gene_ID[0]][1]
+                cnt = 0
+                for i in range(len(gene_ID)):
+                    if(gene_dict[gene_ID[i]][1] == score):
+                        cnt += 1
+                    else:
+                        rank_ave =  int(begin + 1 + cnt /2)
+                        for j in range(begin, i):
+                            output_file.write(str(rank_ave) + "\t" + gene_dict[gene_ID[j]][0] + "\t" + str(gene_ID[j]) + "\t" + str( gene_dict[gene_ID[j]][1]  ) + "\t" + gene_dict[gene_ID[j]][2] + "\n" )
+                            
+                        score = gene_dict[gene_ID[i]][1]
+                        begin = i
+                        cnt = 0
+
+                if(begin < len(gene_ID)):
+                    rank_ave =  int( (begin + len(gene_ID)) /2)
+                    for j in range(begin, len(gene_ID)):
+                        output_file.write(str(rank_ave) + "\t" + gene_dict[gene_ID[j]][0] + "\t" + str(gene_ID[j]) + "\t" + str( gene_dict[gene_ID[j]][1]  ) + "\t" + gene_dict[gene_ID[j]][2] + "\n" )
+                            
---
+++ lib/trim_nl.py
@@ -0,0 +1,4 @@
+def trim_nl(datum):
+    if(datum[-1] == "\n"):
+        datum = datum[0:-1]
+    return datum
