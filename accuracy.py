dea810d5190fd47c24fb1a4f55a3818a772be7a2
Jim Havrilla <semjaavria@gmail.com>
2019-09-24 13:39:54 -0600

    changed tsv, and csh file name

--- accuracy.py
+++ accuracy.py
@@ -51,6 +51,8 @@ sns.set_style('white')
 matplotlib.rcParams['pdf.fonttype'] = 42
 matplotlib.rcParams['font.family'] = 'sans-serif'
 matplotlib.rcParams['font.sans-serif'] = ['Arial']
+matplotlib.rcParams['font.size'] = 16
+
 fig, ax = plt.subplots(1)
 
 width=0.4
@@ -70,7 +72,8 @@ rects=ax.bar(x=alefts,height=phenolyzer,width=width,tick_label=ranks,color=(56/2
 ax.set_xticks(alefts-width*.5)
 autolabel(rects, ax)
 #ax.set_title("Accuracy at Finding Causal Genes")
-ax.legend(loc='upper left')
+#ax.legend(loc='upper left')
+fig.legend(loc='upper left', ncol=1)
 #if max(phenolyzer) > 10:
     #for i,o in enumerate(phenolyzer):
      #   ax.text(alefts[i]-width*.25, 0.14 if o<1 else 5, '%.3f' % phenolyzer[i] if o<1 else '%.1f' % phenolyzer[i], color='k', size=8) #2 for log2
