7695357cf52e9772268b9d623927ef480c15a959
Jim Havrilla <semjaavria@gmail.com>
2019-09-30 15:34:09 -0600

    makes figures now

--- accuracy.sh
+++ accuracy.sh
@@ -1,8 +1,9 @@
 # this will generate Figure 3, the accuracy comparison for benchmark sets
 # between Phen2Gene and Original Phenolyzer
 #### WILL SAVE TO FOLDER "figures" ####
-python accuracy.py -f acccomparison/csh.tsv -o cshaccuracy
-python accuracy.py -f acccomparison/TAF1.tsv -o TAF1accuracy
-python accuracy.py -f acccomparison/columbiaU.tsv -o columbiaUaccuracy
-python accuracy.py -f acccomparison/dgd.tsv -o dgdaccuracy
-python accuracy.py -f acccomparison/ajhg.tsv -o ajhgaccuracy
+python accuracy.py -f rankings/CSH.tsv -o cshaccuracy
+python accuracy.py -f rankings/TAF1.tsv -o TAF1accuracy
+python accuracy.py -f rankings/CU.tsv -o columbiaUaccuracy
+python accuracy.py -f rankings/DGD.tsv -o dgdaccuracy
+python accuracy.py -f rankings/AJHG.tsv -o ajhgaccuracy
+python accuracy.py -f rankings/AJHG_CSH.tsv -o ajhg_cshaccuracy
