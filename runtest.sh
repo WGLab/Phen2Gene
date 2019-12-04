bc622f64cae9a2308c5ded1610980753001e7281
Zhao <zhaom3@l-1-01.cm.cluster>
2019-12-04 13:44:55 -0500

    updated runtest.sh to mkdir rankings/

--- runtest.sh
+++ runtest.sh
@@ -3,12 +3,7 @@
 
 echo "Running the testing data......"
 mkdir -p rankings
-python KB_test_pipeline.py -f -stat tesitngdata_stat > KB_test_pipeline.log &
-sleep 6m
-echo "Finished."
-echo "tsv files are in rankings/"
-echo "Generating figures......."
-sh accuracy.sh
+python KB_test_pipeline.py -f -stat tesitngdata_stat > KB_test_pipeline.log && echo "Finished." && echo "tsv files are in rankings/" && echo "Generating figures......." && sh accuracy.sh
 echo "Finished"
 
 
