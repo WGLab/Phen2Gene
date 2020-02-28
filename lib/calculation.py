5ace8bd217f3acf23e5670f7053422088d1ad0e9
Zhao <zhaom3@l-1-01.cm.cluster>
2020-02-28 14:59:58 -0500

    change the key in "gene_dict"

--- lib/calculation.py
+++ lib/calculation.py
@@ -25,14 +25,14 @@ def calc(hp_weight_list, verbosity, gene_weight, cutoff):
                     #if(hp in hp_downweight_lst):
                         #data[3] = downweight * float(data[3])
                     gene_id = int(data[2])
-                    if(gene_dict.get(gene_id) == None):
+                    if(gene_dict.get(data[1]) == None):
                         # key:gene_id  value: [gene_name, score, 'seedgene/predicted']}
-                        gene_dict[gene_id] = [data[1], hp_weight_list[hp]*float(data[3]), data[4], 0]
+                        gene_dict[data[1]] = [data[1], hp_weight_list[hp]*float(data[3]), data[4], 0, gene_id]
                     else:
-                        gene_dict[gene_id][1] += hp_weight_list[hp]*float(data[3])
-                        gene_dict[gene_id][3] += 1
-                        if(data[4] == "SeedGene" and gene_dict[gene_id][2] == "Predicted"):
-                            gene_dict[gene_id][2] = "SeedGene"
+                        gene_dict[data[1]][1] += hp_weight_list[hp]*float(data[3])
+                        gene_dict[data[1]][3] += 1
+                        if(data[4] == "SeedGene" and gene_dict[data[1]][2] == "Predicted"):
+                            gene_dict[data[1]][2] = "SeedGene"
 
                     line = HP_file.readline()
         except FileNotFoundError:
@@ -60,18 +60,18 @@ def calc(hp_weight_list, verbosity, gene_weight, cutoff):
             #for line in open(r'./lib/lt10gt0'):
             #    lt10gt0.append(line.rstrip('\n'))
 
-            for gene_id in gene_dict.keys():
-                if(gene_dict[gene_id][0] in top100lst):
-                    gene_dict[gene_id][1] *= 0.5
+            for gene_symbol in gene_dict.keys():
+                if(gene_dict[gene_symbol][0] in top100lst):
+                    gene_dict[gene_symbol][1] *= 0.5
                 #if(gene_dict[gene_id][0] in lt10gt0):
                 #    gene_dict[gene_id][1] *= 2
     if(gene_weight):
-        for geneid in gene_dict.keys():
+        for gene_symbol in gene_dict.keys():
             weight = 0
-            with open('./lib/geneweight/{}'.format(gene_dict[geneid][0]), 'r') as fr:
+            with open('./lib/geneweight/{}'.format(gene_dict[gene_symbol][0]), 'r') as fr:
                 weight = float(fr.read().rstrip('\n'))
 
-            gene_dict[geneid][1] *= weight
+            gene_dict[gene_symbol][1] *= weight
     return gene_dict
 
 
@@ -87,13 +87,13 @@ def calc_simple(hp_weight_list, verbosity):
                 while(line ):
                     data = line.strip("\n").split("\t")
                     gene_id = int(data[2])
-                    if(gene_dict.get(gene_id) == None):
+                    if(gene_dict.get(data[1]) == None):
                         # key:gene_id  value: [gene_name, score, 'seedgene/predicted']}
-                        gene_dict[gene_id] = [data[1], 1, data[4]]
+                        gene_dict[data[1]] = [data[1], 1, data[4], gene_id]
                     else:
-                        gene_dict[gene_id][1] += 1
-                        if(data[4] == "SeedGene" and gene_dict[gene_id][2] == "Predicted"):
-                            gene_dict[gene_id][2] = "SeedGene"
+                        gene_dict[data[1]][1] += 1
+                        if(data[4] == "SeedGene" and gene_dict[data[1]][2] == "Predicted"):
+                            gene_dict[data[1]][2] = "SeedGene"
                     line = HP_file.readline()
         except FileNotFoundError:
             if(verbosity):
